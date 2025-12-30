fff97ad5afbfd1e2f151f7f166e6e34e1bf9baab
alejandrobon484 <alejandrobon484@gmail.com>
2025-12-30 18:16:21 +0100

    Cambio de versión en estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,9 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}", // <--- ¡Esta es la que recupera tus estilos!
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    // Fíjate que el nombre ha cambiado, ahora lleva arroba
+    '@tailwindcss/postcss': {},
   },
-  plugins: [],
 };
+
 export default config;
